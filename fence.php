d9916c53c2b21aa4e2337d790f340e2cfaafa575
Dan Tucker <furymuso@aim.com>
2017-09-24 17:10:41 +0100

    first function

---
+++ fence.php
@@ -0,0 +1,25 @@
+<?php
+
+function baseLength()
+{
+    $post = 0.1;
+    $railing = 1.5;
+
+    $baseLength = ($post + $railing + $post);
+        echo $baseLength;
+}
+
+baseLength();
+
+function
+
+?>
+
+<!DOCTYPE html>
+
+<html>
+<body>
+    <h1></h1>
+    <input type="text" name="">
+</body>
+</html>
